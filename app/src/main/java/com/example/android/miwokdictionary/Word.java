668b8408034adbeef78420333eb7a5f8aab2b06f
Teodora <t_nakova_1991@abv.bg>
2017-10-06 21:29:28 +0300

    associate words with images

--- app/src/main/java/com/example/android/miwokdictionary/Word.java
+++ app/src/main/java/com/example/android/miwokdictionary/Word.java
@@ -3,20 +3,33 @@ package com.example.android.miwokdictionary;
 
 public class Word {
 
-    private String mDefaultTranslation;
-    private String mMiwokTranslation;
+    //TODO defaultTranslation to become int and take resource from strings.xml
+    private String defaultTranslation;
+    private String miwokTranslation;
+    private int imageResourceId;
 
 
     public Word(String defaultTranslation, String miwokTranslation) {
-        mDefaultTranslation = defaultTranslation;
-        mMiwokTranslation = miwokTranslation;
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        this(defaultTranslation, miwokTranslation);
+        this.imageResourceId = imageResourceId;
     }
 
     public String getDefaultTranslation() {
-        return mDefaultTranslation;
+        return this.defaultTranslation;
     }
 
     public String getMiwokTranslation() {
-       return mMiwokTranslation;
+        return this.miwokTranslation;
     }
+
+    public int getImageResourceId() {
+        return this.imageResourceId;
+    }
+
+
 }
